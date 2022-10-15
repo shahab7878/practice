cc95420a7e2e13f0fad56411d33984113659a0d3
shahab rabani <112660734+shahab7878@users.noreply.github.com>
2022-10-15 22:49:19 +0330

    Add files via upload

---
+++ answer 2.py
@@ -0,0 +1,14 @@
+int(input("inter an integer num:"))
+if:
+    (1 < num < 100):
+        print("ok")
+    else:
+        print("out of range")
+num = []
+for i in range(5):
+    a = int(input("number = "))
+    num.append(a)
+print(max(num))
+
+
+
